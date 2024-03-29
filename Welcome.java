35cfe72477563ba4b0821f353cac23642c22e5d5
Shaina.T <151636181+Telenshaina@users.noreply.github.com>
2024-03-29 18:41:21 +0800

    Update Welcome ulit

--- Welcome.java
+++ Welcome.java
@@ -16,7 +16,7 @@ public class Welcome extends JFrame implements ActionListener {
     
     public void initComponents() {
         setTitle("GRWM");
-        setSize(new Dimension(1280, 720));
+        setSize(new Dimension(1280, 720)); // 
 
         setResizable(false);
         setLocationRelativeTo(null);
@@ -29,38 +29,57 @@ public class Welcome extends JFrame implements ActionListener {
     public JPanel mainPanel() {
         JPanel panel = new JPanel(null);
         panel.setSize(new Dimension(1280, 720));
-        panel.setBackground(new Color(0x737373));
+        panel.setBackground(new Color(9,38,53));
 
+        
         panel.add(contentPanel());
+        panel.add(semiPanel());
+
+        return panel;
+    }
+
+    public JPanel semiPanel() {
+        JPanel panel = new JPanel(null);
+        panel.setBounds(15, 15, 1230, 650); // xy , wid ,h
+        panel.setBackground(new Color(127, 160, 177));
 
         return panel;
     }
 
     public JPanel contentPanel() {
         JPanel panel = new JPanel(null);
-        panel.setBounds(30, 30, 1200, 620);
-        panel.setBackground(new Color(0xD9D9D9));
+        panel.setBounds(50, 50, 1160, 580);// 30 30 1200 620
+        panel.setBackground(new Color(244,238,255));
 
         JLabel content = new JLabel("Hey partner!");
-        content.setBounds(415, 200, 980, 100);
-        content.setForeground(Color.BLACK);
-        content.setFont(new Font("Arial", Font.BOLD, 70));
+        content.setBounds(395, 175, 980, 100); // -20
+        content.setForeground(new Color(9,38,53));
+        content.setFont(new Font("Monospace", Font.BOLD, 70));
         panel.add(content);
 
         nameField = new JTextField();
-        nameField.setBounds(420, 310, 400, 20);
+        nameField.setBounds(395, 285, 400, 20);
+        nameField.setBackground(new Color(9,38,53));
+        nameField.setForeground(Color.WHITE);
         panel.add(nameField);
 
         JLabel subtext = new JLabel("Enter your name here");
-        subtext.setBounds(545, 330, 200, 30);
+        subtext.setBounds(520, 305, 200, 30);
         subtext.setForeground(new Color(0x737373));
-        subtext.setFont(new Font("Arial", Font.ITALIC, 15));
+        subtext.setFont(new Font("", Font.ITALIC, 15));
         panel.add(subtext);
 
-        enterButton = new JButton("Enter");
-        enterButton.setBounds(570, 370, 100, 30);
+        enterButton = new JButton("<html><span class='btn-34'>Enter</span></html>");
+        enterButton.setBounds(545, 345, 100, 30);
+        enterButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        enterButton.setBackground(new Color(9,38,53));
+        enterButton.setForeground(Color.WHITE);
         enterButton.addActionListener(this);
         panel.add(enterButton);
+
+
+
+
 
         return panel;
     }
