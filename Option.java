5b62f581641c71e282a8db258b23da827970ce32
Thoby Jim Ralleta <152825042+thobidogs@users.noreply.github.com>
2024-03-31 21:01:36 +0800

    Clean clean

--- Option.java
+++ Option.java
@@ -35,7 +35,8 @@ public class Option extends JPanel implements ActionListener {
         panel.setBounds(30, 30, 1200, 260);
         panel.setBackground(new Color(0xEFE7DD));
 
-        JLabel greetings = new JLabel("Welcome, " + name + "!");
+        //"<html><b><i>Hey, partner!</i></b></html>"
+        JLabel greetings = new JLabel("<html><b><i>Welcome, " + name + "!</i></b></html>");
         greetings.setForeground(new Color(0x764B36));
         greetings.setFont(new Font("Arial", Font.BOLD, 70));
     
@@ -55,32 +56,34 @@ public class Option extends JPanel implements ActionListener {
         panel.setBackground(new Color(0xEFE7DD));
 
         JLabel intro = new JLabel("Get Ready With Me to");
-        intro.setBounds(0, 5, 1200, 30);
+        intro.setBounds(0, 15, 1200, 30);
         intro.setForeground(new Color(0x9B4922));
-        intro.setFont(new Font("Arial", Font.ITALIC, 30));
+        intro.setFont(new Font("Arial", Font.PLAIN, 30));
         intro.setHorizontalAlignment(JLabel.CENTER);
         panel.add(intro);
 
         optionOne = new JButton("Prepare for School");
-        optionOne.setBounds(380, 50, 200, 80);
+        optionOne.setBounds(375, 60, 200, 80);
         optionOne.setBackground(new Color(0x5C3420));
         optionOne.setForeground(Color.WHITE);
         optionOne.setFont(new Font("Arial", Font.BOLD, 13));
+        optionOne.setFocusable(false);
         optionOne.addActionListener(this);
         panel.add(optionOne);
 
         JLabel or = new JLabel("or");
-        or.setBounds(585, 80, 30, 20);
+        or.setBounds(575, 80, 50, 50);
         or.setForeground(new Color(0x9B4922));
-        or.setFont(new Font("Arial", Font.ITALIC, 12));
+        or.setFont(new Font("Arial", Font.PLAIN, 30));
         or.setHorizontalAlignment(JLabel.CENTER);
         panel.add(or);
 
         optionTwo = new JButton("Get Dressed");
-        optionTwo.setBounds(620, 50, 200, 80);
+        optionTwo.setBounds(630, 60, 200, 80);
         optionTwo.setBackground(new Color(0x5C3420));
         optionTwo.setForeground(Color.WHITE);
         optionTwo.setFont(new Font("Arial", Font.BOLD, 13));
+        optionTwo.setFocusable(false);
         optionTwo.addActionListener(this::actionPerformedTwo);
         panel.add(optionTwo);
 
