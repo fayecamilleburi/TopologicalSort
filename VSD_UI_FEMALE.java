e9b8c7f21f53bbb02219c2aa9b31036e102097d6
Shaina.T <151636181+Telenshaina@users.noreply.github.com>
2024-03-31 15:47:15 +0800

    Changed back index Update VSD_UI_FEMALE.java

--- VSD_UI_FEMALE.java
+++ VSD_UI_FEMALE.java
@@ -437,7 +437,7 @@ public class VSD_UI_FEMALE extends JFrame implements ActionListener {
             if (panelIndex != -1) {
                 JLabel label = (JLabel) panel.getComponent(0); // Assuming the JLabel is the first component
                 String panelText = label.getText();
-                resultsArea.append(panelIndex+1 + ". " + panelText + "\n");
+                resultsArea.append(panelIndex + ". " + panelText + "\n");
             }
         }
     }
